67477d1c8ad22409b2a6f89b732ac3bd49fdd821
ea234 <anyang@gmx.de>
2026-03-27 12:37:40 +0100

    Day 10 - Part 2

--- src/day_10__Cathod_Ray_Tube.ts
+++ src/day_10__Cathod_Ray_Tube.ts
@@ -8,26 +8,61 @@ import * as readline from 'readline';
  * 
  * 
  * /home/ea234/.nvm/versions/node/v20.16.0/bin/node ./dist/day10/day_10__Cathod_Ray_Tube.js
+ * 
  * Day 10 - Cathod-Ray Tube
  * 
- * Start - Cycle-Sum     0 ADD X    0  X-Register 1  Instruction noop
+ * Start - Cycle-Sum     0 ADD X   15  X-Register 1  Instruction addx 15 - Sprite MIN 0 MAX 2
  * Tick  - Cycle-Sum     1  X 1
- * End   - Cycle-Sum     1 ADD X    0  X-Register 1  Instruction noop
- * 
- * Start - Cycle-Sum     1 ADD X    3  X-Register 1  Instruction addx 3
  * Tick  - Cycle-Sum     2  X 1
- * Tick  - Cycle-Sum     3  X 1
- * End   - Cycle-Sum     3 ADD X    3  X-Register 4  Instruction addx 3
+ * End   - Cycle-Sum     2 ADD X   15  X-Register 16  Instruction addx 15
  * 
- * Start - Cycle-Sum     3 ADD X   -5  X-Register 4  Instruction addx -5
- * Tick  - Cycle-Sum     4  X 4
- * Tick  - Cycle-Sum     5  X 4
- * End   - Cycle-Sum     5 ADD X   -5  X-Register -1  Instruction addx -5
+ * Start - Cycle-Sum     2 ADD X  -11  X-Register 16  Instruction addx -11 - Sprite MIN 15 MAX 17
+ * Tick  - Cycle-Sum     3  X 16
+ * Tick  - Cycle-Sum     4  X 16
+ * End   - Cycle-Sum     4 ADD X  -11  X-Register 5  Instruction addx -11
  * 
- * END - Cycle-Sum     5 ADD X   -5  X-Register -1
+ * Start - Cycle-Sum     4 ADD X    6  X-Register 5  Instruction addx 6 - Sprite MIN 4 MAX 6
+ * Tick  - Cycle-Sum     5  X 5
+ * Tick  - Cycle-Sum     6  X 5
+ * End   - Cycle-Sum     6 ADD X    6  X-Register 11  Instruction addx 6
  * 
+ * ...
  * 
+ * Start - Cycle-Sum   237 ADD X    0  X-Register 17  Instruction noop - Sprite MIN 16 MAX 18
+ * Tick  - Cycle-Sum   238  X 17
+ * End   - Cycle-Sum   238 ADD X    0  X-Register 17  Instruction noop
  * 
+ * Start - Cycle-Sum   238 ADD X    0  X-Register 17  Instruction noop - Sprite MIN 16 MAX 18
+ * Tick  - Cycle-Sum   239  X 17
+ * End   - Cycle-Sum   239 ADD X    0  X-Register 17  Instruction noop
+ * 
+ * Start - Cycle-Sum   239 ADD X    0  X-Register 17  Instruction noop - Sprite MIN 16 MAX 18
+ * Tick  - Cycle-Sum   240  X 17
+ * End   - Cycle-Sum   240 ADD X    0  X-Register 17  Instruction noop
+ * 
+ * END - Cycle-Sum   240 ADD X    0  X-Register 17
+ * 
+ * Result Part 1 = 13140
+ * 
+ * ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
+ * ###   ###   ###   ###   ###   ###   ###
+ * ####    ####    ####    ####    ####
+ * #####     #####     #####     #####
+ * ######      ######      ######      ####
+ * #######       #######       #######
+ * 
+ * Day 10 - Ende
+ * 
+ * Result Part 1 = 14420
+ * 
+ * ###   ##  #    ###  ###  ####  ##  #  #
+ * #  # #  # #    #  # #  #    # #  # #  #
+ * #  # #    #    #  # ###    #  #  # #  #
+ * ###  # ## #    ###  #  #  #   #### #  #
+ * # #  #  # #    # #  #  # #    #  # #  #
+ * #  #  ### #### #  # ###  #### #  #  ##
+ * 
+ * RGLRBZAU
  */
 
 const CYCLES_ADD_X : number = 2;
@@ -55,17 +90,24 @@ function pad( pInput : string | number, pPadLeft : number ) : string
 
 function calcArray( pArray: string[], pKnzDebug : boolean = true ) : void 
 {
-    let result_part_01 : number = 0;
-    let result_part_02 : number = 0;
+    let result_part_01      : number = 0;
+    let result_part_02      : number = 0;
 
-    let x_register_value   : number = 1;
-    let cycles_sum         : number = 0;
-    let cycles_instruction : number = 0;
+    let x_register_value    : number = 1;
+    let cycles_sum          : number = 0;
+    let cycles_instruction  : number = 0;
 
-    let x_register_add : number = 0;
+    let x_register_add      : number = 0;
     
-    let break_point_cycles : number = 20;
-    let break_point_add    : number = 40;
+    let break_point_cycles  : number = 20;
+    let break_point_add     : number = 40;
+
+    let sprite_pos_min      : number = 1;
+    let sprite_pos_max      : number = 1;
+
+    let crt_line_vector     : string[] = [];
+    let crt_line_cur        : string = "";
+    let crt_col_nr          : number = 0;
 
     let vector_break_points : number[] = [ 20, 60, 100, 140, 180, 220 ];
     
@@ -75,7 +117,7 @@ function calcArray( pArray: string[], pKnzDebug : boolean = true ) : void
         {
             cycles_instruction = CYCLES_ADD_X;
 
-            x_register_add = parseInt( cur_input_str.substring( 5 ));
+            x_register_add = parseInt( cur_input_str.substring( 5 ) );
         }
         else if ( cur_input_str.startsWith( "noop" ) )
         {
@@ -84,36 +126,108 @@ function calcArray( pArray: string[], pKnzDebug : boolean = true ) : void
             x_register_add = 0;
         }
 
-        wl( "" );
-        wl( "Start - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value + "  Instruction "  + cur_input_str );
+        /*
+         * Set the sprite min and max position
+         */
+        sprite_pos_min = x_register_value - 1;
+        sprite_pos_max = x_register_value + 1;
 
+        if ( pKnzDebug )
+        {
+            wl( "" );
+            wl( "Start - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value + "  Instruction "  + cur_input_str  + " - Sprite MIN " + ( sprite_pos_min  )  +  " MAX " + ( sprite_pos_max  )  );
+        }
+
+        /*
+         * Do the Cycles
+         */
         for ( let tick_count = 0; tick_count < cycles_instruction; tick_count++ )
         {
+            /*
+             * Add 1 to the cycle sum
+             */
             cycles_sum++;
 
+            /*
+             * Part 1
+             * If the cycle_sum hits a break point, the result for part 1 is increased.
+             */
             if ( vector_break_points.includes( cycles_sum ) )
             {
                result_part_01 += ( x_register_value * cycles_sum );
 
-                wl( "Break - Cycle-Sum " + pad( cycles_sum, 5 ) + "  X " + x_register_value + " =  " + ( x_register_value * cycles_sum ) );
+                if ( pKnzDebug )
+                {
+                    wl( "Break - Cycle-Sum " + pad( cycles_sum, 5 ) + "  X " + x_register_value + " =  " + ( x_register_value * cycles_sum ) );
+                }
             }
             else
             {
-                wl( "Tick  - Cycle-Sum " + pad( cycles_sum, 5 ) + "  X " + x_register_value );
+                if ( pKnzDebug )
+                {
+                    wl( "Tick  - Cycle-Sum " + pad( cycles_sum, 5 ) + "  X " + x_register_value  );
+                }
+            }
+
+            /*
+             * Part 2
+             * If the crt line number is in the range of the sprite positions, * a "#" character 
+             * is added to the crt_line, otherwise a " " Character is added.
+             */
+            if ( ( crt_col_nr >= sprite_pos_min ) && ( crt_col_nr <= sprite_pos_max ) )
+            {
+                crt_line_cur += "#";
+            }
+            else 
+            {
+                crt_line_cur += " ";
+            }
+
+            /*
+             * Increase the crt_col_nr by one.
+             */
+            crt_col_nr++;
+
+            /*
+             * If the crt_col_nr reaches 40, then the current crt_line 
+             * is added to the crt_line-vector. The crt_col_nr is set 
+             * to 0.
+            */
+            if ( crt_col_nr == 40 )
+            {
+               crt_line_vector.push( crt_line_cur );
+
+               crt_line_cur = "";
+
+               crt_col_nr = 0;
             }
         }
 
+        /*
+         * After the instruction cycles the x register is increased. 
+         */
         x_register_value += x_register_add;
 
-        wl( "End   - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value + "  Instruction "  + cur_input_str );
+        if ( pKnzDebug )
+        {
+            wl( "End   - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value + "  Instruction "  + cur_input_str );
+        }
+    }
+
+    if ( pKnzDebug )
+    {
+        wl( "" );
+        wl( "END - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value );
     }
 
     wl( "" );
-    wl( "END - Cycle-Sum " + pad( cycles_sum, 5 ) + " ADD X " + pad( x_register_add, 4 ) + "  X-Register " + x_register_value );
-
-    wl( "" );
     wl( "Result Part 1 = " + result_part_01 );
-    wl( "Result Part 2 = " + result_part_02 );
+    wl( "" );
+
+    for ( let val_crt of crt_line_vector )
+    {
+        wl( val_crt );
+    }
 }
 
 
@@ -321,7 +435,7 @@ function getTestArray2() : string[]
 wl( "Day 10 - Cathod-Ray Tube" );
 wl( "" );
 
-//calcArray( getTestArray2(), true );
+calcArray( getTestArray2(), true );
 
 checkReaddatei();
 
